47b00d22d592b23e835a16b47bfe498f49723518
Suman Bajani <128512864+coder-suman@users.noreply.github.com>
2026-01-15 12:39:18 +0530

    Add files via upload

---
+++ personal_details.java
@@ -0,0 +1,43 @@
+
+//name,age,course,institute
+import java.util.Scanner;
+
+class Personal {
+    Scanner sc = new Scanner(System.in);
+    private String name;
+    private int age;
+    private String course;
+    private String institute;
+
+    void take_input() {
+        System.out.println("----------Enter Your Details------------");
+        System.out.print("Enter Your Name: ");
+        this.name = sc.nextLine();
+        System.out.print("Enter Your Age: ");
+        this.age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter Your Course Name: ");
+        this.course = sc.nextLine();
+        System.out.print("Enter Your Institute: ");
+        this.institute = sc.nextLine();
+        System.out.println("-----------------------------------");
+    }
+
+    void display_empdetails() {
+        System.out.println("--------------Personal Details-------------");
+        System.out.printf("\nName: %s", name);
+        System.out.printf("\nAge: %d", age);
+        System.out.printf("\nCourse Name: %s", course);
+        System.out.printf("\nInstitution Name: %s", institute);
+        System.out.println("\n---------------------------------------------");
+    }
+
+}
+
+public class personal_details {
+    public static void main(String[] args) {
+        Personal p = new Personal();
+        p.take_input();
+        p.display_empdetails();
+    }
+}
