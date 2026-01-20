b2ed5fd914db153653b0da9350d2d56e1b840ba2
Suman Bajani <128512864+coder-suman@users.noreply.github.com>
2026-01-20 13:42:57 +0530

    Add files via upload

---
+++ assignment_6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Num_series{
+    int a=0;
+    int [] arr;
+    int sum=0;
+    double avg=0;
+    Num_series(int a){
+        this.a=a;
+        arr=new int[a];
+    }
+    void gen_number_series(){
+        for(int i=0;i<a;i++){
+            arr[i]=i+2;
+        }
+    }
+    void display_series(){
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        } System.out.println();
+    }
+    void sum_avg_number(){
+        for(int num:arr){
+            sum=sum+num;
+        }
+        System.out.println("The sum of all number is: "+sum);
+        avg=sum/arr.length;
+        System.out.println("The average of all number: "+avg);
+    }
+} 
+public class assignment_6 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("How meny number you want to generate: ");
+        int num=sc.nextInt();
+        Num_series ns=new Num_series(num);
+        ns.gen_number_series();
+        ns.display_series();
+        ns.sum_avg_number();
+    }
+}
